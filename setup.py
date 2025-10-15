e402107028f5a7697dc35a2868f0b3cd838bc03f
Narne674 <bharadwaj.narne.bn@gmail.com>
2025-10-15 17:09:24 -0500

    Code Skeleton Updated

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
 
+def get_requrirements(file_path:str)->list:
+    with open(file_path) as file:
+        requirements = file.readlines()
+        requirements = [req.replace("\n","") for req in requirements]
+        
+        if "-e ." in requirements:
+            requirements.remove("-e .")
+    return requirements
+
 setup(
-    name="deep_learning_project",
+    name="XRay Image Clasification",
     version="0.1.0",
+    author ="Narne",
     packages=find_packages(),
-    install_requires=[],
+    install_requires=get_requrirements("C:\\Users\\bhara\\DeepLearningProject\\requirements_dev.txt")
 )
